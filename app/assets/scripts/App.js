08aeb58681e72d5149a33927f544f205aa64eaf9
senkasija <ivan.jevdjovic@gmail.com>
2020-07-22 11:15:16 +0200

    Completed Header Area

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,10 @@
 
 import "../styles/styles.css"//uvezijemo App sa fajlom za stilove i on će biti bandlovan
+import MobileMenu from "./modules/MobileMenu";
 //import RevealOnScroll from './modules/RevealOnScroll'; //uvozimo modul koji je zadužen za efekte prilikom skrolovanja
+
+let mobileMenu = new MobileMenu();
+//Kreiramo novi objekat kome je prototip klasa MobileMenu
 
 //let revealOnScroll = new RevealOnScroll(); //ovde kreiramo objekat koja će biti korišćen prilikom skrolovanja
 
