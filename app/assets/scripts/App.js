b9309f90c81e81c4cc2d26a7903eaa03f69f5947
senkasija <ivan.jevdjovic@gmail.com>
2020-07-22 22:02:18 +0200

    Completed  Reveal on Scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,14 +1,19 @@
 
 import "../styles/styles.css"//uvezijemo App sa fajlom za stilove i on će biti bandlovan
 import MobileMenu from "./modules/MobileMenu";
-//import RevealOnScroll from './modules/RevealOnScroll'; //uvozimo modul koji je zadužen za efekte prilikom skrolovanja
+import RevealOnScroll from "./modules/RevealOnScroll"; //uvozimo modul koji je zadužen za efekte prilikom skrolovanja
+
+
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75); 
+//ovde kreiramo objekat koja će biti korišćen prilikom skrolovanja
+// u slučaju ovakvog kreiranja objekta, unećemo parametre u konstruktor els for elements tako da će querySelectorAll biti parametar
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+// ovo je kod koji reguliše da se osvežava stranica on the fly uz pomoć webpack-dev-servera
+//drugi parametar je procenat prozora koji ostaje do mesta gdde je ciljani element, kada će se dati element i pojaviti
 
 let mobileMenu = new MobileMenu();
-//Kreiramo novi objekat kome je prototip klasa MobileMenu
+// Kreiramo novi objekat kome je prototip klasa MobileMenu
 
-//let revealOnScroll = new RevealOnScroll(); //ovde kreiramo objekat koja će biti korišćen prilikom skrolovanja
-
-// ovo je kod koji reguliše da se osvežava stranica on the fly uz pomoć webpack-dev-servera
 if(module.hot){
     module.hot.accept();
 }
--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,53 +1,83 @@
 import throttle from 'lodash/throttle'
+/* uvozimo throttle funkcionalnost Lodaša, pri čemu koristimo dva argumenta: funkciju koju hoćemo da pokrenemo i a drugi  je koliko milisekundi želimo da čekamo pre nego što se funkcija pokrene*/
 import debounce from 'lodash/debounce'
+/*  
+  * Ovu Lodašovu alatku uvozimo za slučaj da se uradi resize prozora
+  * The Debounce technique allow us to “group” multiple sequential calls in a single one.
+*/
 
 class RevealOnScroll {
-  constructor(els, thresholdPercent) {
-    this.thresholdPercent = thresholdPercent
-    this.itemsToReveal = els
-    this.browserHeight = window.innerHeight
-    this.hideInitially()
-    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this)
-    this.events()
+  constructor(els, thresholdPercent) { //ovde unosimo parametre sa stranice App.js
+    this.thresholdPercent = thresholdPercent // ova varijabla memoriše drugi parametar tj, procenat vidljivog procenta do trenutka kada će se pojaviti ciljani element
+    this.itemsToReveal = els;  //ova varijabla memoriše html element: document.querySelectorAll(".feature-item")
+    this.hideInitially();
+    //ova funkcija skrivan na početku pojave feture-item i čini ga skrivenim
+    this.browserHeight = window.innerHeight;
+    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
+    /**
+     * bind je sad metod funkcije throttle koja je u stvari objekat. Njim se vraća kopija funkcije throttle
+     * Da bi se pozvala ova kopija funkcije throttle, treba pozvati this.scrollThrottle
+     * this je ova klasa RevealOnScroll
+     * na taj način se u kopiju funnkcije throttle uvoze propertiji ove klase
+    */ 
+    //throttle ima dva argumenta: funkciju i vreme
+    console.log("Šta je this?");
+    console.log(this);
+    this.events();
   }
 
   events() {
-    window.addEventListener("scroll", this.scrollThrottle)
+    window.addEventListener("scroll", this.scrollThrottle);
+        //dodajemo event  scroll na svaki feature-item primenjuje metodu this.scrollThrottle  
     window.addEventListener("resize", debounce(() => {
-      console.log("Resize just ran")
-      this.browserHeight = window.innerHeight
-    }, 333))
+    console.log("Resize just run");
+    this.browserHeight = window.innerHeight;
+    }, 333)) //ovde čekamo ovoliko sekundi kad se prestane raditi resizing
   }
 
   calcCaller() {
     console.log("Scroll function ran")
     this.itemsToReveal.forEach(el => {
-      if (el.isRevealed == false) {
-        this.calculateIfScrolledTo(el)
+      if(el.isRevealed == false){
+        this.calculateIfScrolledTo(el);
       }
+      //poziva se ova metoda za svaki element niza po jednom
     })
   }
 
-  calculateIfScrolledTo(el) {
-    if (window.scrollY + this.browserHeight > el.offsetTop) {
-      console.log("Element was calculated")
-      let scrollPercent = (el.getBoundingClientRect().top / this.browserHeight) * 100
-      if (scrollPercent < this.thresholdPercent) {
-        el.classList.add("reveal-item--is-visible")
-        el.isRevealed = true
-        if (el.isLastItem) {
-          window.removeEventListener("scroll", this.scrollThrottle)
-        }
+calculateIfScrolledTo(el) {//izračunava gde se element nalazi
+
+  if(window.scrollY + this.browserHeight > el.offsetTop){
+    //The offsetTop property returns the top position (in pixels) relative to the top of the offsetParent element.
+    console.log("Element was calculated");
+    let scrollPercent = (el.getBoundingClientRect().top / this.browserHeight) * 100;
+    // izračunavammo procenat koji vrh feature-item zauzima u odnosu na visinu prozora
+    /**
+         * The getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+         * el je feature-item - DAKLE, računamo koliko je gorenji levi ugao feature-item udaljena od gornjeg levog ugla ekrana
+         * getBoundingClientRect().y - ovo ne radi u Edgu, već treba napisati getBoundingClientRect().top
+         * Za uštedu memorije postoji opcija: Intersection Observer - It would allow us to code this feature while using much less computing resources while scrolling. 
+    */
+    if (scrollPercent < this.thresholdPercent) {
+      el.classList.add("reveal-item--is-visible");
+      // ako zauzme manje od 75% onda će se pojaviti
+      el.isRevealed = true;
+      //ukoliko je je true vidljiv je element feature-item
+      if (el.isLastItem) {
+        window.removeEventListener("scroll", this.scrollThrottle);//kad dođemo do poslednjeg elementa, uklanjamo eventListener;
       }
     }
   }
+}
 
   hideInitially() {
     this.itemsToReveal.forEach(el => {
-      el.classList.add("reveal-item")
-      el.isRevealed = false
+      el.classList.add("reveal-item");
+      //dodaje stil svakom elementu feature-item
+      el.isRevealed = false;
+      //ukoliko je je false skriven je element feature-item
     })
-    this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true
+   this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true; //Ovim kodom dohvatam poslednji element niza feature-item i dajem tom objektu properti isLastItem i vrednost true:
   }
 }
 
