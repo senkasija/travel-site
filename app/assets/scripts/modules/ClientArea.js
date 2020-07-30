1d2562b0468ecb0ba618635640d0b3848453eee8
Ivan Jevđović <ivan.jevdjovic@gmail.com>
2020-07-30 22:34:16 +0200

    New knowledge feature  about communication with server side.

--- app/assets/scripts/modules/ClientArea.js
+++ app/assets/scripts/modules/ClientArea.js
@@ -3,16 +3,16 @@ import Axios from 'axios'
 class ClientArea {
   constructor() {
     this.injectHTML()
-    this.form = document.querySelector(".client-area__form")
-    this.field = document.querySelector(".client-area__input")
-    this.contentArea = document.querySelector(".client-area__content-area")
+    this.form = document.querySelector(".client-area__form"); //Ovde memorišemo šta se dešava u formi: submited...
+    this.field = document.querySelector(".client-area__input"); // Ovde memorišemo šta se dešava u polju imput
+    this.contentArea = document.querySelector(".client-area__content-area"); // Ovde memorišemo prazan div u kome će se pojaviti poruka o uspehu ili grešci.
     this.events()
   }
 
-  events() {
+  events() { // Ovde se osluškuje da li se dešava neki događaj u formi.
     this.form.addEventListener("submit", e => {
-      e.preventDefault()
-      this.sendRequest()
+      e.preventDefault(); // OVde sprečavamo refrešovanje stranice
+      this.sendRequest(); // Ako se pritisne submit, šalje se zahtev koji definišemo u donjoj metodi. Možemo izabrati bilo koju reč.
     })
   }
 /**
@@ -20,21 +20,23 @@ class ClientArea {
  */
   sendRequest() {
     /**
-     * Axios.post metod rezultuje obećanjem. Suština obećanja je da ne znamo koliko će trajati izvršavanje, ali na kraju mora da rezultuje ili uspehom ili neuspehom.
-     * U then ideo opcija sa uspehom, a u catch sa neuspehom.
-     * Prima dva argumenta: url kome šaljemo zahtev, a drugi je objekat u kome je jedan property password sa vrednošću koju ukuca korisnik.
+     * Ovde bi išla metoda fetch, ali autor voli Axios
+     * Axios.post metod obezbeđuje obećanje. Suština obećanja je da ne znamo koliko će trajati izvršavanje, ali na kraju mora da rezultuje ili uspehom ili neuspehom, ili čekanjem na kraju krajeva.
+     * U then ide opcija sa uspehom, a u catch sa neuspehom.
+     * Prima dva argumenta: url u kome šaljemo zahtev, a drugi je objekat u kome je jedan property password sa vrednošću koju ukuca korisnik. Ovaj drugi properti je dakle JS koji se tokom slanja parsira u JSON
      * ne može se testirati lokalno, ali u lekciji ima uputstvo
      */
     Axios.post('https://vibrant-mcnulty-a86518.netlify.app/.netlify/functions/secret-area', {password: this.field.value}).then(response => {
-      this.form.remove();
-      this.contentArea.innerHTML = response.data//ovo je vrednost kojom odgovara cloud function: dobrodošli u tajnu oblast
+      this.form.remove();//ako je tačan rezultat briše formu i unosi tekst koji smo ppripremili za uspešan scenario
+      this.contentArea.innerHTML = response.data//ovo je vrednost kojom odgovara cloud function: dobrodošli u tajnu oblast i data je rezervisan reč koliko vidim. To je onaj tekst u HTMLU.
     }).catch(() => {
       this.contentArea.innerHTML = `<p class="client-area__error">That secret phrase is not correct. Try again.</p>`
-      this.field.value = ''//briše automatski netačan unos
+      this.field.value = ''//briše automatski netačan unos tako da ne moramo pritiskati backspace
       this.field.focus()//ostavlja ga u fokusu
     })
   }
-
+//The insertAdjacentHTML() method inserts a text as HTML, into a specified position.
+//
   injectHTML() {
     document.body.insertAdjacentHTML('beforeend', `
     <div class="client-area">
--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,10 +1,22 @@
+/*
+* Callback funkcija služi da nas obavesti o uspehu našeg zahteva
+* U Secret Cntentu možemo da pišemo kod koji niko ne može da vidi
+* Zajedno sa slanjem zahteva putem URL-a, mi šaljemo i vrednost koju smo uneli u polje
+* Vrednost koju šaljemo pišemo u JASONu i to pišemo u Postmenu: body - raw, a biramo opciju POST, a onda na dnu ekrana vidimo odgovor: a to iz varijable secretContent. Ovako Postmenom simuliramo slanje podataka koje unosi korisnik, i vraćanje odgovora korisniku.
+* Parametar event sadrži informacije o zahtevu koji šaljemo. On ekstraktuje podatke koje korisnik šalje.
+* Ako event.body postoje onda treba da podatke, koji su uvek u stringu, parsira (pretvori) iz JSONA  u kom one stižu u JS objekt koji je nama potreban.
+* A ako nema odgovora, onda je body prazan objekat.
+* Ako je user poslao tačne podatke, onda dobija secretContent, a ako nije poruku o grešci 401
+* body je sa JS objekat koji se pravi od JSONA
+
+*/
 exports.handler = function(event, context, callback) {
+
     const secretContent = `
     <h3>Welcome To The Secret Area</h3>
     <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
     `
-    
-    let body
+    let body;
   
     if (event.body) {
       body = JSON.parse(event.body)
